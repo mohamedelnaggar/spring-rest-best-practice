6f3fd4d3e45afa62c2bcc26a9b38b67610e42128
mohamed elnaggar <mohamed.nn90@gmail.com>
2019-10-04 16:38:25 +0200

    Added payment controller

---
+++ src/main/java/com/silicon/payment/PaymentServiceApp.java
@@ -0,0 +1,13 @@
+package com.silicon.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApp.class, args);
+    }
+
+}
